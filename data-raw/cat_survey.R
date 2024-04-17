9f76f2a19dc3e10ba9e5238bc38973c9271fcecf
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-04-17 07:55:09 -0700

    use integers for questions

--- data-raw/cat_survey.R
+++ data-raw/cat_survey.R
@@ -13,4 +13,15 @@ cat_survey <- read_xlsx("data-raw/Feline_dataset.xlsx", sheet = 2) |>
     problematic_behavior = fct(problematic_behavior, na = "unknown")
   )
 
+not_questions <- c(
+  "catid", "breedgroup", "sex", "age_behaviour", "other_cats",
+  "problematic_behavior", "fearfulness", "human_aggression",
+  "activity_playfulness", "cat_sociability", "human_sociability",
+  "litterbox_issues", "excessive_grooming_2"
+)
+
+cat_survey <- cat_survey |>
+  mutate(across(c(everything(), -any_of(not_questions)), as.integer)) |>
+  glimpse()
+
 usethis::use_data(cat_survey, overwrite = TRUE)
